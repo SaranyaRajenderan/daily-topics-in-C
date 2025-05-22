7f58c545d142b80c21f478e6127734e82ed110f4
SaranyaRajenderan <saranyathree19@gmail.com>
2025-05-22 11:02:45 +0530

    Add files via upload

---
+++ DAY 7/Pattern.C
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=1;i<3;i++)
+     for(int j=1;j<6;j++){
+       if (i%2==0) printf("hi");
+       if (j%2==0) printf("\nhello");
+}} 
